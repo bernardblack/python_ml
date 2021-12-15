4e48fcb9b0b76d25f19f9072e8a9309b2d18e9f2
Bernard <bernmbox@gmail.com>
2021-12-15 13:50:09 +0200

    Hello

--- main.py
+++ main.py
@@ -1,30 +1,6 @@
-li = [[1, 3, 3, 4], [2, 1, 3, 5], [4, 0, 1, 7], [5, 2, 1, 0], [0, 4, 8, 3]]
-d = {}
-s = set()
-
-
 def main():
 
-    print(li)
-
-    li.sort(key=lambda x: x[1])
-    print(li)
-
-    for i in li:
-        d[i.pop(1)] = i
-    print(d)
-
-    for v in d.values():
-        v.sort(reverse=True)
-    print(d)
-
-    for k in d:
-        for v in d[k]:
-            s.add(v)
-    print(s)
-
-    string = str(s)
-    print(string)
+    print("Hello, world!")
 
 
 if __name__ == "__main__":
